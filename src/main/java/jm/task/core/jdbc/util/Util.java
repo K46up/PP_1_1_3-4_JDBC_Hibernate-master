685821cb23e33d5a84f7c95169bc5f247253a971
Meu3 <k46up@yandex.ru>
2024-02-05 19:58:55 +0300

    Connect DB

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -11,11 +11,15 @@ public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/db_pp_1";
     private static final String USER = "root";
     private static final String PASSWORD = "java";
-    private static Connection connection; //= DriverManager.getConnection(URL, USER, PASSWORD);
+    private static Connection connection;
 
-    public static Connection getConnection() throws SQLException {
-        if (connection == null) {
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+    public static Connection getConnection() {
+        try {
+            if (!connection.isClosed()) {
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
         return connection;
     }
