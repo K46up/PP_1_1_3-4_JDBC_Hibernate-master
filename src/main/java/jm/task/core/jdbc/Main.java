d22c88e7636c56e8859c993acf0ac130d6b3e4e0
Meu3 <k46up@yandex.ru>
2024-02-05 06:02:59 +0300

    Connect DB

---
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,21 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+        // реализуйте алгоритм здесь
+
+/*        try {
+            Util.getConnection();
+            System.out.println("Connect");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        Util.closeConnection();
+        System.out.println("Disconnect");*/
+    }
+}
---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,30 @@
+package jm.task.core.jdbc.util;
+
+import javax.sql.ConnectionEvent;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/db_pp_1";
+    private static final String USER = "root";
+    private static final String PASSWORD = "java";
+    private static Connection connection; //= DriverManager.getConnection(URL, USER, PASSWORD);
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null) {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
